dbad1827b58a29773b5dfcd494d3ec719e182997
gngtrash <gng.trash@gmail.com>
2022-10-09 22:19:46 +0900

    mvvm

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace avalonia_play
 {
@@ -7,6 +9,25 @@ namespace avalonia_play
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        string buttonText = "Click!!";
+        public string ButtonText
+        {
+            get => buttonText;
+            set
+            {
+                buttonText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonText)));
+            }
+        }
+
+        public void ButtonClicked() => ButtonText = "Clicked!!!";
     }
 }
