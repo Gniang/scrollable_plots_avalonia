ae53304df0624a880a50c89a30c28432105c055b
gngtrash <gng.trash@gmail.com>
2022-10-09 20:52:39 +0900

    avalonia

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace avalonia_play
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
